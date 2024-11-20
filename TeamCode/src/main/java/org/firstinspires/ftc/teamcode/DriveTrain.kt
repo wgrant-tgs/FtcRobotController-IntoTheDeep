f3fcf3deaeb5a55ff33cb2a871e779ab532829af
Lucas Watkins <114666925+lucas-watkins@users.noreply.github.com>
2024-11-20 15:21:46 -0500

    Fix my bad math

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveTrain.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveTrain.kt
@@ -41,10 +41,10 @@ class DriveTrain : BaseLinearOpMode() {
             val turnPower = -this.gp1.current.right_trigger + this.gp1.current.left_trigger
 
             val motorPower = arrayOf(
-                this.gp1.current.left_stick_x - this.gp1.current.left_stick_y + turnPower,
-                -this.gp1.current.left_stick_x - this.gp1.current.left_stick_y - turnPower,
-                -this.gp1.current.left_stick_x - this.gp1.current.left_stick_y + turnPower,
                 this.gp1.current.left_stick_x - this.gp1.current.left_stick_y - turnPower,
+                -this.gp1.current.left_stick_x - this.gp1.current.left_stick_y + turnPower,
+                -this.gp1.current.left_stick_x - this.gp1.current.left_stick_y - turnPower,
+                this.gp1.current.left_stick_x - this.gp1.current.left_stick_y + turnPower,
             )
 
             // Magnitude of the maximum value, not velocity
