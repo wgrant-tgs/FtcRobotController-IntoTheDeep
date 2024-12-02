0e12c0c3b371712073fd1ec7c5c94defb8f1987f
tildejustin <spacepepper3.14@gmail.com>
2024-12-02 12:00:34 -0500

    bunch of random odometry changes and additions

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/modular/BaseLinearOpMode.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/modular/BaseLinearOpMode.kt
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.teamcode.modular
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode
 import com.qualcomm.robotcore.hardware.DcMotor
 import com.qualcomm.robotcore.hardware.DcMotorSimple
+import org.firstinspires.ftc.teamcode.GoBildaPinpointDriver
 
 @Suppress("MemberVisibilityCanBePrivate")
 abstract class BaseLinearOpMode : LinearOpMode() {
@@ -10,7 +11,9 @@ abstract class BaseLinearOpMode : LinearOpMode() {
     protected lateinit var rightBackMotor: DcMotor
     protected lateinit var rightFrontMotor: DcMotor
     protected lateinit var leftFrontMotor: DcMotor
+    protected lateinit var odometry: GoBildaPinpointDriver
     protected lateinit var allMotors: Array<DcMotor>
+
 
     protected fun initDriveTrain() {
         try {
@@ -25,6 +28,15 @@ abstract class BaseLinearOpMode : LinearOpMode() {
             this.rightBackMotor.direction = DcMotorSimple.Direction.FORWARD
 
             this.allMotors = arrayOf(leftFrontMotor, rightFrontMotor, leftBackMotor, rightBackMotor)
+
+            this.odometry = this.hardwareMap.get(GoBildaPinpointDriver::class.java, "odometry")
+            this.odometry.setOffsets(12.0, 6.0)
+            this.odometry.setEncoderResolution(74.5027025)
+            this.odometry.setEncoderDirections(
+                GoBildaPinpointDriver.EncoderDirection.FORWARD,
+                GoBildaPinpointDriver.EncoderDirection.REVERSED
+            )
+            odometry.resetPosAndIMU()
 
             telemetry.addData("Drive Train Initialization", "Success")
 
