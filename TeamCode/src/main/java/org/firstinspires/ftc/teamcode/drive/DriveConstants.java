40eeab1ee3451d24ef59e087e8da6f112c1ace4e
wgrant-tgs <147338880+wgrant-tgs@users.noreply.github.com>
2024-12-30 13:28:17 -0500

    Base, unteseted, constants for roadrunner

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
@@ -21,8 +21,10 @@ public class DriveConstants {
     /*
      * These are motor constants that should be listed online for your motors.
      */
-    public static final double TICKS_PER_REV = 1;
-    public static final double MAX_RPM = 1;
+
+    // We use Gobilda 5201s
+    public static final double TICKS_PER_REV = 1497.325; // I believe TICKS_PER_REV is encoder resolution ("Encoder Cycles Per Revolution" as listed in gobilda website), but I could be wrong
+    public static final double MAX_RPM = 105;
 
     /*
      * Set RUN_USING_ENCODER to true to enable built-in hub velocity control using drive encoders.
@@ -32,7 +34,7 @@ public class DriveConstants {
      * If using the built-in motor velocity PID, update MOTOR_VELO_PID with the tuned coefficients
      * from DriveVelocityPIDTuner.
      */
-    public static final boolean RUN_USING_ENCODER = false;
+    public static final boolean RUN_USING_ENCODER = true; // they probably are broken but we should really use them
     public static PIDFCoefficients MOTOR_VELO_PID = new PIDFCoefficients(0, 0, 0,
             getMotorVelocityF(MAX_RPM / 60 * TICKS_PER_REV));
 
@@ -44,9 +46,11 @@ public class DriveConstants {
      * angular distances although most angular parameters are wrapped in Math.toRadians() for
      * convenience. Make sure to exclude any gear ratio included in MOTOR_CONFIG from GEAR_RATIO.
      */
-    public static double WHEEL_RADIUS = 2; // in
-    public static double GEAR_RATIO = 1; // output (wheel) speed / input (motor) speed
-    public static double TRACK_WIDTH = 1; // in
+
+    // Take the following with a grain of salt
+    public static double WHEEL_RADIUS = 1.8898; // in
+    public static double GEAR_RATIO = 19.2; // output (wheel) speed / input (motor) speed
+    public static double TRACK_WIDTH = 16.5; // in
 
     /*
      * These are the feedforward parameters used to model the drive motor behavior. If you are using
@@ -65,10 +69,10 @@ public class DriveConstants {
      * small and gradually increase them later after everything is working. All distance units are
      * inches.
      */
-    public static double MAX_VEL = 30;
-    public static double MAX_ACCEL = 30;
-    public static double MAX_ANG_VEL = Math.toRadians(60);
-    public static double MAX_ANG_ACCEL = Math.toRadians(60);
+    public static double MAX_VEL = 90;
+    public static double MAX_ACCEL = 90;
+    public static double MAX_ANG_VEL = Math.toRadians(286.4788975654116);
+    public static double MAX_ANG_ACCEL = Math.toRadians(286.4788975654116);
 
     /*
      * Adjust the orientations here to match your robot. See the FTC SDK documentation for details.
