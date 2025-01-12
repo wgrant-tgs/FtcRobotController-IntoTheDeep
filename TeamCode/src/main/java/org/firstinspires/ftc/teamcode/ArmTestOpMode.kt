5c23787ffb381f7b552132671561d40a4e13df38
tildejustin <spacepepper3.14@gmail.com>
2025-01-12 10:40:11 -0500

    change some hardware imports

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ArmTestOpMode.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ArmTestOpMode.kt
@@ -1,14 +1,19 @@
 package org.firstinspires.ftc.teamcode
 
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp
+import com.qualcomm.robotcore.hardware.CRServo
 import com.qualcomm.robotcore.hardware.DcMotor
+import com.qualcomm.robotcore.hardware.DcMotorEx
 import com.qualcomm.robotcore.hardware.DcMotorSimple
 import com.qualcomm.robotcore.hardware.Gamepad
+import com.qualcomm.robotcore.hardware.Servo
+import com.qualcomm.robotcore.hardware.TouchSensor
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit
 import org.firstinspires.ftc.teamcode.modular.BaseLinearOpMode
 import org.firstinspires.ftc.teamcode.modular.GamepadButton
 import org.firstinspires.ftc.teamcode.modular.GamepadState
+import kotlin.math.sign
 
 @Suppress("unused")
 @TeleOp(name = "Arm Test")
@@ -16,28 +21,61 @@ class ArmTestOpMode : BaseLinearOpMode() {
     override fun runOpMode() {
         this.telemetry.msTransmissionInterval = 100
         this.initDriveTrain()
-        var arm = this.hardwareMap.dcMotor["arm"]
-        var elevator = this.hardwareMap.dcMotor["elevator"]
+        var arm = this.hardwareMap["arm"] as DcMotorEx
         arm.direction = DcMotorSimple.Direction.REVERSE
         arm.mode = DcMotor.RunMode.RUN_USING_ENCODER
-        elevator.mode = DcMotor.RunMode.RUN_USING_ENCODER
-        var switch = this.hardwareMap.touchSensor["touch.sensor"]
+        var elevator = this.hardwareMap["elevator"] as DcMotorEx
+        elevator.direction = DcMotorSimple.Direction.REVERSE
+        elevator.mode = DcMotor.RunMode.RUN_WITHOUT_ENCODER
+        var ratchet = ServoWrapper(this.hardwareMap["ratchet"] as Servo, 0.1, 0.0)
+        ratchet.inactivate()
+        val leftSpinner = this.hardwareMap["leftSpinner"] as CRServo
+        leftSpinner.direction = DcMotorSimple.Direction.REVERSE
+        val rightSpinner = this.hardwareMap["rightSpinner"] as CRServo
+        val spinner = Spinner(leftSpinner, rightSpinner)
+        val bucket = this.hardwareMap["bucketServo"] as Servo
+        var switch = this.hardwareMap["touch.sensor"] as TouchSensor
         var lastSwitch = switch.isPressed
         var gp1 = GamepadState(this.gamepad1)
-        var up = GamepadButton(gp1, Gamepad::dpad_up)
-        var down = GamepadButton(gp1, Gamepad::dpad_down)
+        val gp2 = GamepadState(this.gamepad2)
+        val rd = GamepadButton(gp2, Gamepad::dpad_right)
         this.waitForStart()
         while (this.opModeIsActive()) {
             gp1.cycle()
+            gp2.cycle()
+
             val currSwitch = switch.isPressed
+
+            // arm reaches bottom
             if (currSwitch && !lastSwitch) {
                 arm.mode = DcMotor.RunMode.STOP_AND_RESET_ENCODER
                 arm.mode = DcMotor.RunMode.RUN_USING_ENCODER
+                ratchet.activate()
+                Thread.sleep(500)
+                bucket.position = 0.225
             }
-            if (up.isPressed && !down.isPressed && arm.currentPosition < 7000) arm.power = 0.2
-            else if (down.isPressed && !up.isPressed && !currSwitch) arm.power = -0.2
-            else arm.power = 0.0
-            elevator.power = -gp1.current.right_stick_y.toDouble() * 0.2
+
+            // going up while arm is at bottom
+            if (ratchet.engaged() && (-gp2.current.right_stick_y > 0 && currSwitch || -gp2.current.right_stick_y < 0 && !currSwitch /* should never happen */)) {
+                ratchet.inactivate()
+                Thread.sleep(500)
+            }
+
+            // arm going up
+            if (-gp2.current.right_stick_y > 0 && arm.currentPosition < 7000) {
+                arm.power = -gp2.current.right_stick_y * 0.5
+                // going down
+            } else if (-gp2.current.right_stick_y < 0 && !currSwitch) {
+                bucket.position = 0.5
+                arm.power = -gp2.current.right_stick_y * 0.5
+            } else arm.power = 0.0
+
+            // elevator and spinner
+            elevator.power = gp2.current.left_stick_y.toDouble()
+            if (gp2.current.left_stick_y.sign != 0f) spinner.on(gp2.current.left_stick_y > 0) else spinner.off()
+
+            rd.ifIsToggled { bucket.position = 0.0 }
+
             lastSwitch = currSwitch
 
             this.telemetry.addData("arm pos", arm.currentPosition)
@@ -46,7 +84,7 @@ class ArmTestOpMode : BaseLinearOpMode() {
               gets the current Position (x & y in mm, and heading in degrees) of the robot, and prints it.
             */
             val pos = odometry.position
-            val data = "{X: %.3f, Y: %.3f, H: %.3f}".format(
+            val data = "X: %.3f, Y: %.3f, H: %.3f".format(
                 pos.getX(DistanceUnit.MM),
                 pos.getY(DistanceUnit.MM),
                 pos.getHeading(AngleUnit.DEGREES)
@@ -57,7 +95,7 @@ class ArmTestOpMode : BaseLinearOpMode() {
             gets the current Velocity (x & y in mm/sec and heading in degrees/sec) and prints it.
              */
             val vel = odometry.velocity
-            val velocity = "{XVel: %.3f, YVel: %.3f, HVel: %.3f}".format(
+            val velocity = "XVel: %.3f, YVel: %.3f, HVel: %.3f".format(
                 vel.getX(DistanceUnit.MM),
                 vel.getY(DistanceUnit.MM),
                 vel.getHeading(AngleUnit.DEGREES)
@@ -78,6 +116,39 @@ class ArmTestOpMode : BaseLinearOpMode() {
             //prints/gets the current refresh rate of the Pinpoint
             this.telemetry.addData("Pinpoint Frequency", odometry.frequency)
             this.telemetry.update()
+        }
+    }
+
+    class Spinner(val left: CRServo, val right: CRServo) {
+        fun on(inwards: Boolean) = set(if (inwards) 1.0 else -1.0)
+        fun off() = set(0.0)
+        fun get() = left.power == 0.0
+        fun set(num: Double) {
+            left.power = num;
+            right.power = num;
+        }
+    }
+
+    // active ~ closed
+    class ServoWrapper(val servo: Servo, val active: Double, val inactive: Double) {
+        var toggled = false;
+
+        fun toggle(): Boolean {
+            toggled = !toggled
+            servo.position = if (toggled) active else inactive
+            return toggled
+        }
+
+        fun engaged() = toggled
+
+        fun activate() {
+            toggled = true
+            servo.position = active
+        }
+
+        fun inactivate() {
+            toggled = false;
+            servo.position = inactive
         }
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/modular/BaseLinearOpMode.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/modular/BaseLinearOpMode.kt
@@ -1,26 +1,26 @@
 package org.firstinspires.ftc.teamcode.modular
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode
-import com.qualcomm.robotcore.hardware.DcMotor
+import com.qualcomm.robotcore.hardware.DcMotorEx
 import com.qualcomm.robotcore.hardware.DcMotorSimple
 import org.firstinspires.ftc.teamcode.GoBildaPinpointDriver
 
 @Suppress("MemberVisibilityCanBePrivate")
 abstract class BaseLinearOpMode : LinearOpMode() {
-    protected lateinit var leftBackMotor: DcMotor
-    protected lateinit var rightBackMotor: DcMotor
-    protected lateinit var rightFrontMotor: DcMotor
-    protected lateinit var leftFrontMotor: DcMotor
+    protected lateinit var leftBackMotor: DcMotorEx
+    protected lateinit var rightBackMotor: DcMotorEx
+    protected lateinit var rightFrontMotor: DcMotorEx
+    protected lateinit var leftFrontMotor: DcMotorEx
     protected lateinit var odometry: GoBildaPinpointDriver
-    protected lateinit var allMotors: Array<DcMotor>
-
+    protected lateinit var allMotors: Array<DcMotorEx>
 
     protected fun initDriveTrain() {
+        // TODO: find a way to print this exception while blocking starting the program
         try {
-            this.leftBackMotor = this.hardwareMap.dcMotor["leftRear"]
-            this.rightBackMotor = this.hardwareMap.dcMotor["rightRear"]
-            this.rightFrontMotor = this.hardwareMap.dcMotor["rightFront"]
-            this.leftFrontMotor = this.hardwareMap.dcMotor["leftFront"]
+            this.leftBackMotor = this.hardwareMap["leftRear"] as DcMotorEx
+            this.rightBackMotor = this.hardwareMap["rightRear"] as DcMotorEx
+            this.rightFrontMotor = this.hardwareMap["rightFront"] as DcMotorEx
+            this.leftFrontMotor = this.hardwareMap["leftFront"] as DcMotorEx
 
             this.leftFrontMotor.direction = DcMotorSimple.Direction.REVERSE
             this.rightFrontMotor.direction = DcMotorSimple.Direction.FORWARD
@@ -29,7 +29,7 @@ abstract class BaseLinearOpMode : LinearOpMode() {
 
             this.allMotors = arrayOf(leftFrontMotor, rightFrontMotor, leftBackMotor, rightBackMotor)
 
-            this.odometry = this.hardwareMap.get(GoBildaPinpointDriver::class.java, "odometry")
+            this.odometry = this.hardwareMap["odometry"] as GoBildaPinpointDriver
             this.odometry.setOffsets(95.0, 0.0)
             this.odometry.setEncoderResolution(37.25135125)
             this.odometry.setEncoderDirections(
